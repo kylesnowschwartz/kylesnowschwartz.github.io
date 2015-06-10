d358d46b8e69493161f4e2b94ed59e13e79964c3
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-06-10 16:41:27 +1200

    added personalized styling to conway

--- conway_js/javascripts/index.js
+++ conway_js/javascripts/index.js
@@ -1,17 +1,31 @@
 $(document).ready(function() {
-  var conway = new Conway(65);
-  conway.renderGrid();
 
+  function RunConway() {
+    var conway = new Conway(65);
+    conway.renderGrid();
+    var time = setInterval( function () {
+      conway.updateNeighborsForAllCells();
+      conway.updateAllCells();
+    }, 100);
+  }
+
+  $(document).on('click', '.refresh-conway', function (e) {
+    $('#grid').html('');
+    RunConway();
+  });
+
+  RunConway();
+
+});
+
+
+// when we resize the window, we want to change the width and height of each cell,
+// we want to make the row height to equal cell height
   // var windowWidth = $(window).width();
   // var cellSize = windowWidth / 100;
   // $('.cell').width(cellSize);
   // $('.cell').height(cellSize);
   // $('.row').height(cellSize);
-
-  var time = setInterval( function () {
-    conway.updateNeighborsForAllCells();
-    conway.updateAllCells();
-  }, 100);
 
   // $(window).resize(function() {
   //   var windowWidth = $(window).width();
@@ -20,9 +34,3 @@ $(document).ready(function() {
   //   $('.cell').height(cellSize);
   //   $('.row').height(cellSize);
   // });
-
-});
-
-
-// when we resize the window, we want to change the width and height of each cell,
-// we want to make the row height to equal cell height
