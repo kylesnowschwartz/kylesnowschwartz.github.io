6c1716be2a74b20503c78e380c5e8d7b7b28c35a
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-06-26 22:47:37 +1200

    marmitegasm

--- keyboardography/js/application.js
+++ keyboardography/js/application.js
@@ -13,7 +13,7 @@
 // goal 4:
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1000 );
 var renderer = new THREE.WebGLRenderer();
 var controls = new THREE.OrbitControls( camera );
 
@@ -22,10 +22,10 @@ renderer.setClearColor( 0xffffff, 1);
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry( 1, .1, 1 );
-var material = new THREE.MeshBasicMaterial( {color: 0xd3d3d3} );
-var cube = new THREE.Mesh( geometry, material );
-var cubeEdges = new THREE.EdgesHelper( cube, 0xffffff);
+// var geometry = new THREE.BoxGeometry( 1, .1, 1 );
+// var material = new THREE.MeshBasicMaterial( {color: 0xd3d3d3} );
+// var cube = new THREE.Mesh( geometry, material );
+// var cubeEdges = new THREE.EdgesHelper( cube, 0xffffff);
 
 var row1letters = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"]
 var row2letters = ["a", "s", "d", "f", "g", "h", "j", "k", "l"]
@@ -80,11 +80,19 @@ $(document).on("keypress", function(e) {
   }
 });
 
-camera.position.z = 8;
-camera.position.y = 5;
+
+// camera.position.z = 8;
+// camera.position.y = 6;
+camera.position.set(0,0,10);
+// camera.up = new THREE.Vector3(2,1,20);
+// camera.lookAt(new THREE.Vector3(0,0,0));
+
 
 function render() {
   requestAnimationFrame( render );
+
+  // cube.rotation.x += 0.1;
+  // cube.rotation.y += 0.1;
 
   renderer.render( scene, camera );
 }
