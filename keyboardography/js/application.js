64bdbd757a055c534e19545d361a1c3fa6103583
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-07-30 20:22:44 +1200

    lowercased letters in image file names

--- keyboardography/js/application.js
+++ keyboardography/js/application.js
@@ -29,9 +29,9 @@ document.body.appendChild( renderer.domElement );
 
 
 var $1234 = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "="]
-var qwerty = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]"]
-var asdf = ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'"]
-var zxcv = ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"]
+var qwerty = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]"]
+var asdf = ["a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'"]
+var zxcv = ["z", "x", "c", "v", "b", "n", "m", ",", ".", "/"]
 var keys = {}
 
 
